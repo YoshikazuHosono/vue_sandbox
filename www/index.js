af1da0f1b018e6182a5045033efd9aaf2dcd06c8
YoshikazuHosono <yyhhwork@gmail.com>
2022-01-20 20:12:04 +0900

    save

--- www/index.js
+++ www/index.js
@@ -4,3 +4,12 @@ var app = new Vue({
         message: 'Hello Vue!'
     }
 })
+
+app.message = 'Hello Vue!!!!!!!!!!!';
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})

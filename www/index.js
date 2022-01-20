f8997c747deb6886b20668c668a94140c2b6a3e6
YoshikazuHosono <yyhhwork@gmail.com>
2022-01-20 20:13:56 +0900

    v-if

--- www/index.js
+++ www/index.js
@@ -13,3 +13,11 @@ var app2 = new Vue({
     message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 })
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen : false
+  }
+})
+app3.seen = true;

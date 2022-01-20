0239c2785cf62929bf7baf12510b2c577b572369
YoshikazuHosono <yyhhwork@gmail.com>
2022-01-20 20:35:48 +0900

    component

--- www/index.js
+++ www/index.js
@@ -1,5 +1,8 @@
 Vue.component('todo-item', {
-  template: '<li>This is a todo</li>'
+  // todo-item コンポーネントはカスタム属性のような "プロパティ" で受け取ります。
+  // このプロパティは todo と呼ばれます。
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
 })
 
 var app = new Vue({
@@ -60,4 +63,11 @@ var app6 = new Vue({
 
 var app7 = new Vue({
   el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
 })

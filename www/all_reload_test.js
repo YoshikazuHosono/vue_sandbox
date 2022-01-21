b2326fb39e67298db38d5030c7cf84403f217cd7
YoshikazuHosono <yyhhwork@gmail.com>
2022-01-21 19:40:28 +0900

    update reload sample

--- www/all_reload_test.js
+++ www/all_reload_test.js
@@ -2,6 +2,7 @@ new Vue({
     el: '#app',
     data: {
       current: new Date(),
+      current2: new Date(),
     },
     computed: {
       random: function(){
@@ -15,5 +16,8 @@ new Vue({
       onclickUpdateCurrentTimestamp(){
         this.current = new Date();
       },
+      onclickUpdateCurrentTimestamp2(){
+        this.current2 = new Date();
+      }
     }
 })

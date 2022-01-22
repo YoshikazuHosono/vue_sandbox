099cc9207cbe6a17b3ca2d93c773d8e636338f3a
YoshikazuHosono <yyhhwork@gmail.com>
2022-01-22 20:15:31 +0900

    end chapter 1,2

--- www/js/watch.js
+++ www/js/watch.js
@@ -9,11 +9,16 @@ new Vue({
       },
       godName: 'HOSONO',
       trueGodName: '',
+      inputValue1: '',
+      updatedInputValue1: '',
     },
     created() {
       this.unwatch = this.$watch('godName',function(newValue,oldValue){
         this.trueGodName = '真・' + this.godName;
       });
+    },
+    beforeDestroy() {
+      this.unwatch();
     },
     watch: {
       name(newValue,oldValue) {
@@ -22,5 +27,12 @@ new Vue({
       'autor.name'(newValue,oldValue) {
         this.autor.company = newValue + ' ultimate.inc';
       },
+      inputValue1: {
+        handler: function(newValue,oldValue) {
+          this.updatedInputValue1 = 'updated [' + newValue + ']';
+        },
+        deep: true,
+        immediate: false,
+      }
     },
 })

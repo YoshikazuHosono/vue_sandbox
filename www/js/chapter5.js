a4b837c806e6e6e0cdc74e98dbd774004fe5eac2
YoshikazuHosono <yyhhwork@gmail.com>
2022-01-25 23:34:52 +0900

    save

---
+++ www/js/chapter5.js
@@ -0,0 +1,30 @@
+Vue.component('banner-hosono', {
+  template: `
+  <div class="banner">
+    <h3>チームHOSONOメンバー卍募集中！！！</h3>
+    <p>異世界転生経験者の多い弊社で、在宅LEGO審判としての経験をイカしてみませんか？</p>
+  </div>
+  `,
+});
+
+Vue.component('banner-yoshikazu', {
+  template: `
+  <div class="banner">
+    <h3>YOSHIKAZUを探しています、、、</h3>
+    <p>1000年の宴が終焉を迎えしとき、みたいな雰囲気のサイゼリアの壁の絵</p>
+  </div>
+  `,
+});
+
+Vue.component('banner-jack', {
+  template: `
+  <div class="banner">
+    <h3>よぉ、JACK、、、</h3>
+    <p>XYZ</p>
+  </div>
+  `,
+});
+
+let app = new Vue({
+  el: '#app',
+});
